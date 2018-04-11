ade923dcfef11878cd16e7c3f79324276d8d16e9
Diego <diego@geeny.io>
2018-04-12 00:59:07 +0200

    Adding simple admin

--- web.py
+++ web.py
@@ -1,8 +1,32 @@
-
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.admin import Admin
+from flask.ext.admin.contrib.sqla import ModelView
+
 app = Flask(__name__)
+# Create dummy secrey key so we can use sessions
+app.config['SECRET_KEY'] = '123456790'
+
+# Create in-memory database
+app.config['DATABASE_FILE'] = 'sample_db.sqlite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
 
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(128))
+
+class Sensor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+
+if __name__ == '__main__':
+    admin = Admin(app)
+    admin.add_view(ModelView(User, db.session))
+    admin.add_view(ModelView(Sensor, db.session))
+
+    db.create_all()
+    app.run('0.0.0.0', 8000)

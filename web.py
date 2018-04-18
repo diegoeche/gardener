076540d9adb348cfe415e5cf249afe752f426f73
Diego <diego@geeny.io>
2018-04-18 23:44:14 +0200

    simple template rendering, added datatypes

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.admin import Admin
 from flask.ext.admin.contrib.sqla import ModelView
@@ -13,7 +13,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
@@ -22,11 +21,24 @@ class User(db.Model):
 class Sensor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    user = db.relationship('User', backref=db.backref('users', lazy=True))
+
+class SensorData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    value = db.Column(db.Float)
+    sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'))
+    sensor = db.relationship('Sensor', backref=db.backref('sensors', lazy=True))
+
+@app.route('/')
+def dashboard():
+    return render_template('dashboard.html', name="Diego")
 
 if __name__ == '__main__':
     admin = Admin(app)
     admin.add_view(ModelView(User, db.session))
     admin.add_view(ModelView(Sensor, db.session))
+    admin.add_view(ModelView(SensorData, db.session))
 
     db.create_all()
     app.run('0.0.0.0', 8000)

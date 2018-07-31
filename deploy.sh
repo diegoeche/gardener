1ed4f9a9732e0a30cf81fd3d02b0bdd15403eab8
Diego <diego@geeny.io>
2018-07-31 21:49:33 +0200

    Changing deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,7 @@
 cd /home/pi/gardener
 git pull origin master
 sudo systemctl stop gardener
+echo "Service Stoped"
 sudo systemctl start gardener
+echo "Service Started"
+sudo systemctl status gardener
